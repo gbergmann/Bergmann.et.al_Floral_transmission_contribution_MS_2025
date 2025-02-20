6a1b51facff5d44f83ee6dc14053bd346d4f6ada
Gillian Bergmann <gbergmann@ucdavis.edu>
2025-02-19 17:25:18 -0800

    started adding code to stigma inoculation experiment script

--- scripts/Stigma_analysis.R
+++ scripts/Stigma_analysis.R
@@ -1,1 +1,15 @@
 # Analysis of stigma inoculation experiment
+
+#### loading packages and data ####
+library(tidyverse)
+library(cowplot)
+library(pheatmap)
+library(reshape2)
+library(plotly)
+library(ggpubr)
+library(chisq.posthoc.test)
+library(viridis)
+
+# data files
+slice.turbidity <- read.csv("data_raw/Stigma_inoc/")
+fruit.abortion <- read.csv("data_raw/Stigma_inoc/")
